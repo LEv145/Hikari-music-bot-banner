aff2d41b3b759a20f32281c3f474912f5fa0dc19
LEv145 <40071054+LEv145@users.noreply.github.com>
2022-01-27 19:58:37 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+   name='hikari_musicbot_banner',
+   version='1.0',
+   description='A useful module',
+   author='LEv145',
+   packages=['hikari_musicbot_banner'],
+   include_package_data=True,
+)
